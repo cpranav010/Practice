4344947232c60a2f9658b693133b9331c328683e
Pranav Chavan <pranavchavan09@gmail.com>
2023-08-23 00:13:33 +0530

    First

---
+++ src/p.java
@@ -0,0 +1,23 @@
+class p extends c
+{
+	
+	p(String i) {
+		super(i);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Hello");
+	}
+}
+class c 
+{
+	c(int o)
+	{		
+	}
+
+	 c(String i) {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
